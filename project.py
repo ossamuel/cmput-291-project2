7950f0bfab07a7a908833d83b98f2e124df249ac
jmonteza <26825560+jmonteza@users.noreply.github.com>
2020-11-20 23:22:54 -0700

    Update project.py

--- project.py
+++ project.py
@@ -3,9 +3,13 @@ import datetime
 from pymongo import MongoClient, ASCENDING, DESCENDING
 import json
 import re
+from beautifultable import BeautifulTable
+
+ANONYMOUS = True
 
 # Mongoclient object (Online)
-client = MongoClient("mongodb+srv://cmput291:4B5VzRRSNz81cvqz@cmput291.7yrbk.mongodb.net/<dbname>?retryWrites=true&w=majority")
+client = MongoClient(
+    "mongodb+srv://cmput291:4B5VzRRSNz81cvqz@cmput291.7yrbk.mongodb.net/<dbname>?retryWrites=true&w=majority")
 
 # Name of the database
 db = client["291db"]
@@ -15,56 +19,6 @@ postCol = db["Posts"]
 tagsCol = db["Tags"]
 votesCol = db["Votes"]
 
-# Insert into collection
-# post1 =  {
-#         "Id": "1",
-#         "PostTypeId": "1",
-#         "AcceptedAnswerId": "9",
-#         "CreationDate": "2010-08-17T19:22:37.890",
-#         "Score": 16,
-#         "ViewCount": 28440,
-#         "Body": "<p>What is the hardware and software differences between Intel and PPC Macs?</p>\n",
-#         "OwnerUserId": "10",
-#         "LastEditorUserId": "15",
-#         "LastEditDate": "2010-09-08T15:12:04.097",
-#         "LastActivityDate": "2017-09-21T12:16:56.790",
-#         "Title": "What is the difference between Intel and PPC?",
-#         "Tags": "<hardware><mac><powerpc><macos>",
-#         "AnswerCount": 9,
-#         "CommentCount": 0,
-#         "FavoriteCount": 6,
-#         "ContentLicense": "CC BY-SA 2.5"
-#       }
-# postCol.insert_one(post1)
-
-# tag1 =   {
-#         "Id": "4",
-#         "TagName": "hardware",
-#         "Count": 1435,
-#         "ExcerptPostId": "20888",
-#         "WikiPostId": "20887"
-#       }
-
-# tagsCol.insert_one(tag1)
-
-# vote1 = {
-    #     "Id": "1",
-    #     "PostId": "1",
-    #     "VoteTypeId": "2",
-    #     "CreationDate": "2010-08-17T00:00:00.000"
-    #   }
-
-# votesCol.insert_one(vote1)
-
-tags2 = {
-        "Id": "6",
-        "TagName": "snow-leopard",
-        "Count": 1197,
-        "ExcerptPostId": "8990",
-        "WikiPostId": "8989"
-      }
-
-# tagsCol.insert_one(tags2)
 
 def delete_all(collection):
     """
@@ -73,13 +27,16 @@ def delete_all(collection):
     """
     collection.delete_many({})
 
+
 def insert_one(collection, document):
     collection.insert_one(document)
 
-def parse_terms(title="", body=""):    
-    new_string = [i for i in re.split("\s|[,.!?<>()/=:]", title+body) if len(i) > 2 and i != '"']
+
+def parse_terms(title="", body=""):
+    new_string = [i for i in re.split(
+        "\s|[,.!?<>()/=:]", title+body) if len(i) > 2 and i != '"']
     no_duplicate = []
-    
+
     for i in new_string:
         if i not in no_duplicate:
             no_duplicate.append(i)
@@ -87,11 +44,13 @@ def parse_terms(title="", body=""):
     return no_duplicate
 
 # key in ["posts", "tags", "votes"]
+
+
 def fromJsonFile(fileName, key, isPost, collection):
     """
     Reads json file and constructs a collection for each (except for Posts collection)
     """
-    
+
     # open the file
     with open(fileName) as file:
         # load the json
@@ -100,7 +59,7 @@ def fromJsonFile(fileName, key, isPost, collection):
         if isPost:
             count = 0
             for entry in data[key]["row"]:
-                
+
                 title = entry.get("Title", 0)
                 body = entry.get("Body", 0)
                 terms = None
@@ -116,37 +75,36 @@ def fromJsonFile(fileName, key, isPost, collection):
                 # Only body exists
                 elif body:
                     terms = (parse_terms(entry["Body"]))
-                
+
                 termsDict = {"Terms": terms}
-                
+
                 combined = {**entry, **termsDict}
                 postCol.insert_one(combined)
                 postCol.create_index([('Terms', ASCENDING)])
-            
+
         else:
             for entry in data[key]["row"]:
                 # dict type
                 collection.insert_one(entry)
-    
+
     print("Done!")
 
 
-# delete_all(postCol)
 
-<<<<<<< Updated upstream
 def post():
-    '''
-    This function is responsible for giving 
+    """
+    This function is responsible for giving
     user access to make a post given title and body
     text fields are filled and meet proper requirements
-    '''
+    """
+    
     global user_id
     print("\nMAKE A POST")
     title = body = ''
     while True:
         title = input('Please enter a title for the post: \n')
         if format_check(title, 1): break
-    
+
     while True:
         body = input('Please enter a body for the post: \n')
         if format_check(body, 1): break
@@ -154,15 +112,15 @@ def post():
         tag = input('Please enter a tags for the post: \n')
         if tag:
             if format_check(tag, 1): break
-            
+
         else:
-            #no tag entered by user
+            # no tag entered by user
             break
     tag = tag.split()
     n = len(tag)
     s = ""
     for i in range(n):
-        s+="<"+str(tag[i])+">"
+        s += "<"+str(tag[i])+">"
     body = "<p>"+body+"</p>"
     post_q = {
          "Id":  getMAXID(postCol) + 1,
@@ -179,26 +137,27 @@ def post():
          "FavoriteCount": 0,
          "ContentLicense": "CC BY-SA 2.5"
     }
-    postCol.insert_one(post_q) 
+    postCol.insert_one(post_q)
     print('Successfully made a post.\n')
 
 
-#fromJsonFile("Posts.json", "posts", True, postCol)
-#fromJsonFile("Votes.json", "votes", False, votesCol)
-#fromJsonFile("Tags.json", "tags", False, tagsCol)
+# fromJsonFile("Posts.json", "posts", True, postCol)
+# fromJsonFile("Votes.json", "votes", False, votesCol)
+# fromJsonFile("Tags.json", "tags", False, tagsCol)
 # delete_all(votesCol)
 # delete_all(tagsCol)
 
 def getMaxID(collection):
-    return collection.find_one(sort=[($convert:"Id", DESCENDING)])["Id"]
-    
+    return collection.find_one(sort=[("Id", DESCENDING)])["Id"]
+
+
 def answer(questionID, userID):
     """
     Answer the question by providing a text
     """
-    
+
     text = input(f"Enter an answer for {questionID}: ")
-    
+
     answerDict = {
         "Id": str(int(getMaxID(postCol)) + 1),
         "PostTypeId": "2",
@@ -211,25 +170,47 @@ def answer(questionID, userID):
         "CommentCount": 0,
         "ContentLicense": "CC BY-SA 2.5"
       }
-    
+
    # postCol.insert_one(answer)
-    print(">>> Your Answer (id#{}) was added to Question (id#{}).".format(answerDict["Id"], questionID))
+    print(">>> Your Answer (id#{}) was added to Question (id#{}).".format(
+        answerDict["Id"], questionID))
 
-#print(datetime.datetime.now().isoformat())
-#print(postCol)
+# print(datetime.datetime.now().isoformat())
+# print(postCol)
 
 
-def list_answers():
+def list_answers(questionID):
     """
     List answers of a selected question
     """
-    pass
+    answers = postCol.find({"PostTypeId": "2", "ParentId": questionID})
+
+    print("\n{:>50}".format("ANSWERS FOR QUESTION " + questionID))
+
+    table = BeautifulTable()
+
+    table.columns.header = ["Body", "Creation Date", "Score"]
+
+    table.set_style(BeautifulTable.STYLE_BOX)
+
+    accId = int(postCol.find_one({"Id": questionID})["AcceptedAnswerId"])
+
+    acceptedAnswer = postCol.find_one({"Id": str(accId)})
+
+    table.rows.append([acceptedAnswer["Body"][0:80] + " " + "\u2605", acceptedAnswer["CreationDate"], acceptedAnswer["Score"]])
+
+    for answer in answers:
+        if int(answer["Id"]) != accId: 
+            table.rows.append([answer["Body"][0:80], answer["CreationDate"], answer["Score"]])
+    
+    print(table)
 
 def vote():
     """
     Vote on a selected question
     """
 
-#answer(12345, 14141)
-print(getMaxID(tagsCol))
+# answer(12345, 14141)
+# print(getMaxID(tagsCol))
 
+list_answers("1")
